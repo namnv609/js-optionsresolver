ab7aac6fa65210521281db956402f46476b28a78
NamNV609 <namnv609@gmail.com>
2017-08-28 16:26:51 +0700

    Resolver data

--- js-optionsresolver.js
+++ js-optionsresolver.js
@@ -9,6 +9,8 @@ window.OptionsResolver = function() {
   var _allowedTypes = {};
   // Allowed value for options object
   var _allowedValues = {};
+  // Normalizer closures
+  var _normalizers = {};
 
   /**
    * Defined option keys
@@ -177,5 +179,202 @@ window.OptionsResolver = function() {
     }
 
     return _allowedValues;
+  };
+
+  /**
+   * Set normalizer for an option
+   *
+   * @param {String} optionKey     Option key
+   * @param {Function} allowedValues The normalizer
+   * @return {this}
+   */
+  this.setNormalizer = function(optionKey, allowedValues) {
+    _normalizers[optionKey] = allowedValues;
+
+    return this;
+  };
+
+  /**
+   * Merge options with the default values stored in the container and validates them
+   *
+   * @param  {Object} dataObj Data object
+   * @return {Object} The merged and validated options
+   */
+  this.resolve = function(dataObj) {
+    _checkUndefinedOptions(Object.keys(dataObj));
+    dataObj = _setOptionsDefault(dataObj);
+    _checkMissingRequiredOptions(dataObj);
+    _checkInvalidOptionsType(dataObj);
+    _checkInvalidOptionsValue(dataObj);
+    dataObj = _normalizerOptionsValue(dataObj);
+
+    return dataObj;
+  };
+
+  /**
+   * Private functions area
+   */
+
+  /**
+   * Check defined options
+   *
+   * @param  {Array} optionKeys Options
+   * @return {mixed} Void or exception
+   */
+  _checkUndefinedOptions = function(optionKeys) {
+    var knowOptionsStr = "\"" + _definedOptions.join("\", \"") + "\"";
+
+    for (var idx in optionKeys) {
+      var objKey = optionKeys[idx];
+
+      if (_definedOptions.indexOf(objKey) === -1) {
+        throw "UndefinedOptionsException: The option \"" + objKey + "\" does not exist. Know options are: " + knowOptionsStr;
+      }
+    }
+  };
+
+  /**
+   * Check missing required options
+   *
+   * @param  {Object} dataObj Data object
+   * @return {mixed} Void or exception
+   */
+  _checkMissingRequiredOptions = function(dataObj) {
+    for (var idx in _requiredOptions) {
+      var requiredKey = _requiredOptions[idx];
+      var requiredValue = dataObj[requiredKey];
+
+      if (!requiredValue) {
+        throw "MissingOptionsException: The required options \"" + requiredKey + "\" is missing";
+      }
+    }
+  };
+
+  /**
+   * Set default value for options
+   *
+   * @param {Object} dataObj Data object
+   * @return {Object} Data object with default values
+   */
+  _setOptionsDefault = function(dataObj) {
+    Object.keys(_defaultValues).forEach(function(key, idx) {
+      if (!dataObj[key]) {
+        dataObj[key] = _defaultValues[key];
+      }
+    });
+
+    return dataObj;
+  };
+
+  /**
+   * Check value for options is valid type
+   *
+   * @param  {Object} dataObj Data object
+   * @return {mixed} Void or exeception
+   */
+  _checkInvalidOptionsType = function(dataObj) {
+    for (var key in _allowedTypes) {
+      var optionValue = dataObj[key];
+      switch(_allowedTypes[key].toLowerCase()) {
+        case "int":
+          if (typeof optionValue !== "number" || !/^\-?\d+$/.test(optionValue)) {
+            _throwInvalidOptionsTypeException(key, optionValue, "int");
+          }
+          break;
+        case "string":
+          if (typeof optionValue !== "string") {
+            _throwInvalidOptionsTypeException(key, optionValue, "string");
+          }
+          break;
+        case "array":
+          if (!optionValue || optionValue.constructor !== Array) {
+            _throwInvalidOptionsTypeException(key, optionValue, "array");
+          }
+          break;
+        case "bool":
+        case "boolean":
+          if (typeof optionValue !== "boolean") {
+            _throwInvalidOptionsTypeException(key, optionValue, "boolean")
+          }
+          break;
+        case "float":
+          if (typeof optionValue !== "number" || !/^\-?\d+\.\d+$/.test(optionValue)) {
+            _throwInvalidOptionsTypeException(key, optionValue, "float");
+          }
+          break;
+        case "object":
+          if (typeof optionValue !== "object" || optionValue.constructor !== Object) {
+            _throwInvalidOptionsTypeException(key, optionValue, "object");
+          }
+          break;
+        case "regexp":
+          if (typeof optionValue !== "object" || optionValue.constructor !== RegExp) {
+            _throwInvalidOptionsTypeException(key, optionValue, "regexp");
+          }
+          break;
+        case "fn":
+        case "func":
+        case "function":
+          if (typeof optionValue !== "function" || optionValue.constructor !== Function) {
+            _throwInvalidOptionsTypeException(key, optionValue, "function");
+          }
+          break;
+      }
+    }
+  };
+
+  /**
+   * Check value is valid allowed data
+   *
+   * @param  {Object} dataObj Data object
+   * @return {mixed} Void or exeception
+   */
+  _checkInvalidOptionsValue = function(dataObj) {
+    for (var key in _allowedValues) {
+      var optionValue = dataObj[key];
+      var optionAllowedValues = _allowedValues[key];
+      var isValidValue = true;
+      var acceptedValueMessage = "";
+
+      if (optionAllowedValues && optionAllowedValues.constructor === Array) {
+        isValidValue = (optionAllowedValues.indexOf(optionValue) >= 0);
+        acceptedValueMessage = " Accepted values are: \"" + optionAllowedValues.join("\", \"") + "\"";
+      } else if (optionAllowedValues && optionAllowedValues.constructor === Function) {
+        isValidValue = optionAllowedValues(optionValue);
+      } else {
+        isValidValue = (optionValue === optionAllowedValues);
+        acceptedValueMessage = " Accepted value are \"" + optionAllowedValues + "\""
+      }
+
+      if (!isValidValue) {
+        throw "The option \"" + key + "\" with value \"" + optionValue + "\" is invalid." + acceptedValueMessage;
+      }
+    }
+  };
+
+  /**
+   * Normalizer data for options
+   *
+   * @param  {Object} dataObj Data object
+   * @return {Object} Data object with normalized data
+   */
+  _normalizerOptionsValue = function(dataObj) {
+    for (var key in _normalizers) {
+      dataObj[key] = _normalizers[key](dataObj, dataObj[key]);
+    }
+
+    return dataObj;
   }
+
+  /**
+   * Throw exeception for check valid option type
+   *
+   * @param  {String} key          Option key
+   * @param  {mixed} value         Option value
+   * @param  {String} expectedType Expected data type
+   * @return {throw} Exception
+   */
+  _throwInvalidOptionsTypeException = function(key, value, expectedType) {
+    throw "InvalidOptionsException: The option \"" + key + "\" with \"" + value + "\" is expected to be of type \"" + expectedType + "\"";
+  };
 };
